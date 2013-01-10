c8f77b4d391b6445ce27a84d39898b3e8bc8d094
Frank Hassanabad <foreverlinux@yahoo.com>
2013-01-09 23:27:12 -0700

    Added list example

--- war/src/main/java/rest/frankhassanabad/com/github/QueryParameter.java
+++ war/src/main/java/rest/frankhassanabad/com/github/QueryParameter.java
@@ -1,8 +1,9 @@
 package rest.frankhassanabad.com.github;
 
 import javax.ws.rs.*;
-import java.awt.*;
+import java.awt.Color;
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * Shows an example of using query parameters.
@@ -33,6 +34,29 @@ public class QueryParameter {
     @Produces("text/plain")
     public String getString(@DefaultValue("Nothing set in the parameter") @QueryParam("param") String param) {
         return "The parameter set was: " + param;
+    }
+
+    /**
+     * Returns a string of the query parameter "param".
+     * If the query param is not passed in then this uses
+     * as default of "Nothing set in the parameter".
+     * Example use is
+     * <pre>
+     * http://localhost:8080/rest/query/strings?param=Here%20is%20one&param=Here%20is%20two
+     * </pre>
+     * @param params The parameter from the "?param=" part of the URI
+     * @return Echos back the parameter
+     */
+    @Path("strings")
+    @GET
+    @Produces("text/plain")
+    public String getStrings(@DefaultValue("Nothing set in the parameter") @QueryParam("param") List<String> params) {
+        StringBuilder returnString = new StringBuilder("The parameters set were:\n");
+        for (String parameter : params) {
+            returnString.append(parameter);
+            returnString.append("\n");
+        }
+        return returnString.toString();
     }
 
     /**
