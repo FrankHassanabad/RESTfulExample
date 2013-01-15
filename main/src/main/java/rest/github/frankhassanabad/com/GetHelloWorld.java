6a221b5f245af563ff9ed22d7b58f13c9327499d
Frank Hassanabad <foreverlinux@yahoo.com>
2013-01-14 20:33:37 -0700

    Updated the get hello world to have another example which utilizes headers

--- main/src/main/java/rest/github/frankhassanabad/com/GetHelloWorld.java
+++ main/src/main/java/rest/github/frankhassanabad/com/GetHelloWorld.java
@@ -1,9 +1,12 @@
 package rest.github.frankhassanabad.com;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.List;
 
 /**
  * This is a client which will get the HelloWorld
@@ -16,12 +19,29 @@ public class GetHelloWorld {
      * @param args None are used
      */
     public static void main(String[] args) {
+
+        //Simple straight forward way to get the hello world response
         Client client = Client.create();
         WebResource resource = client.resource("http://localhost:8080/rest/helloworld");
         String helloWorldResponse = resource.accept(
                 MediaType.TEXT_PLAIN).
                 get(String.class);
         System.out.println(helloWorldResponse);
+
+        //A lower level way of getting the hello world response in which
+        //you can iterate over headers and all kinds of other goodies from
+        //the request
+        ClientResponse response = resource.get(ClientResponse.class);
+        String helloWorldResponseLowLevel = response.getEntity(String.class);
+        System.out.println(helloWorldResponseLowLevel);
+        System.out.println("Headers returned from the server are:");
+        MultivaluedMap<String, String> headers = response.getHeaders();
+        for (String header : headers.keySet()) {
+            List<String> values = headers.get(header);
+            for (String value : values) {
+                System.out.println("    " + header + ":" + value);
+            }
+        }
     }
 
 }
